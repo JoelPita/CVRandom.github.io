a381cabd495af29dee2ddfaa5ef537b02a07b95b
Joel Pita <joeljpita91@gmail.com>
2023-02-02 08:56:03 -0300

    Completada las fichas de experiencias, sin archivo json

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 persona_random();
-persona_experiencia ();
 
 async function persona_random (){
     const response = await fetch('https://randomuser.me/api');
     const archivo_datos = await response.json();
     const ficha_de_persona = archivo_datos.results[0];
     cargar_datos_personales(ficha_de_persona);
-    persona_experiencia ();
 }
-
-async function persona_experiencia (){
-    const response2 = await fetch('/jason.json');
-    const archivo_datos2 = await response2.json();
-    let presentacion = document.getElementById("Presentacion");
-    presentacion.innerHTML = archivo_datos2.presentacion ;
-}
-
 
 function cargar_datos_personales(ficha){
 
     let imagen_foto = document.getElementById("foto");
     imagen_foto.setAttribute('src', `${ficha.picture.large}`);
 
+    //Para el encabezado de datos personales.
     cargar_al_id( `${ficha.name.title} ${ficha.name.first} ${ficha.name.last}` , "nombre-completo");
+
+    //Para la seccion de Experiencias
+    cargar_al_id( `${ficha.name.first} ${ficha.name.last}` , "nombre-apellido");
+    cargar_al_id( `${ficha.location.city}` , "ciudad");
+    cargar_al_id( `${ficha.location.state}` , "estado");
+    
+    //Para la tarjeta de Datos Personales
     cargar_al_id( `${ficha.name.first}` , "DP-nombre")
     cargar_al_id( `${ficha.name.last}` , "DP-apellido")
     cargar_al_id( `${ficha.dob.age}` , "DP-edad")
     cargar_al_id( `${ficha.nat}` , "DP-nacionalidad")
+    
+    //Para la tarjeta de Datos de Contacto
     cargar_al_id( `${ficha.phone}`, "DC-telefono")
     cargar_al_id( `${ficha.cell}`,"DC-celular");
     cargar_al_id( `${ficha.email}` ,"DC-email");
